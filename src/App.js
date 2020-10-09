4fb2d6a59b268af537307a448d58f211386ea116
Ivan Osipau <Ivan.Osipau@itechart-group.com>
2020-10-09 06:26:56 -0700

    fixed out comments, rewriten functions

--- src/App.js
+++ src/App.js
@@ -1,74 +1,63 @@
 import React from 'react'
 import classes from './index.module.css'
 
-
-
-
-
 class App extends React.Component {
    constructor(props) {
-     super(props);
-
+    super(props);
     this.state = {
       newTask: '',
-      taskList: []
+      taskList: [],
     }
    }
-   nextNewTask(){
-    const getTask = document.getElementById('inp').value;
-    console.log(getTask)
+newTask(input){
     this.setState({
-      newTask: getTask
-    });
+        newTask: input
+    })
   }
-
-   addNewTask(){
-     if (this.state.newTask != '') {
-     this.state.taskList.push(this.state.newTask);
-     document.getElementById('inp').value = '';
-     this.setState({
-      newTask: ""
-    });
+addNewTask(input){
+     if (this.state.newTask !== '') {
+          const list = this.state.taskList;
+          list.push(input);
+           this.setState({
+            newTask: '',
+             taskList: list
+           })
   } return
-     console.log(this.state.taskList)
    }
-
-
-
-  render() {
+render() {
     return (
-      <div className={classes.App}>
-        <h1>{this.state.taskList.length} Task today </h1>
-        <div className={classes.contentWrapper}>
-          <div className={classes.tasks}>
+      <div className={ classes.App }>
+        <h1>{ this.state.taskList.length } Task today </h1>
+        <div className={ classes.contentWrapper }>
+          <div className={ classes.tasks }>
             <ol>
-    {this.state.taskList.map((task) => <li className={classes.item}>{task}</li>)}
+             { this.state.taskList.map((task) => 
+             <li className={ classes.item } key={task.toString() + Math.random()} > 
+                   { task }
+                   <input type="checkbox" className={classes.checkBox}/>  
+             </li>) }
             </ol>
             </div>
-            <div className={classes.formContainer}>
+            <div className={ classes.formContainer }>
               <input 
-              id={'inp'}
-              className={classes.input} 
-              onChange={() => this.nextNewTask()} 
-              type='text' 
-              >
+                value={this.state.newTask}
+                className={ classes.input } 
+                onChange={ input => this.newTask(input.target.value) } 
+                type='text'>
               </input>
               <button
-               className={classes.button}
-               onClick={() => this.addNewTask()}
-               >Add task</button>
+                className={ classes.button }
+                onClick={ () => this.addNewTask(this.state.newTask) }
+               >Add task
+              </button>
+            </div>
+            <div className={ classes.buttonsCotainer }>
+                <button className={classes.delButton}>Delete checked task</button>
+                <button className={classes.clearButton} >Clear input</button>
             </div>
         </div>
-
       </div>
     )
   }
 }
-
-
-
-
-
-
 export default App
-
